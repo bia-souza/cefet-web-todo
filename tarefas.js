70b7391632603b297590d4d99b4488b9baf181a3
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-07-16 22:52:46 -0300

    Exercicio 2

--- tarefas.js
+++ tarefas.js
@@ -1,4 +1,5 @@
 listaTarefaQuery = document.querySelector('ul#lista-tarefas');
+botaoAdicionaQuery = document.querySelector('#incluir-nova-tarefa');
 
 let tarefas = [
 	{
@@ -28,3 +29,19 @@ listaTarefaQuery.innerHTML = '';
 for(tarefa of tarefas){
 	insereTarefaNaPagina(tarefa);
 }
+
+function criaTarefa() {
+	nomeQuery = document.querySelector('#nova-tarefa-nome');
+	categoriaQuery = document.querySelector('#nova-tarefa-categoria');
+	tarefa = {
+		nome: `${nomeQuery.value}`,
+		categoria: `${categoriaQuery.selectedIndex.value}`,
+		marcado: false
+	}
+	tarefas.push(tarefa);
+	insereTarefaNaPagina(tarefa);
+	nomeQuery.value = '';
+	nomeQuery.focus();
+}
+
+botaoAdicionaQuery.addEventListener('click', criaTarefa);
